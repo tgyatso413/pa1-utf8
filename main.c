4d57524722c5bcb5aa95b91fca43808230a285fa
Tsering Gyatso <31828246+tgyatso413@users.noreply.github.com>
2024-10-08 07:12:23 +0000

    Starting UTF Analyzer

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 int32_t is_ascii(unsigned char str[]) {
     for (int i = 0; i < strlen(str); i += 1) {
@@ -21,11 +22,69 @@ int32_t capitalize_ascii(char str[]) {
     return x;
 }
 
-int main() {
-    printf("Is 🔥 ASCII? %d\n", is_ascii("🔥"));
+int32_t width_from_start_byte(char start_byte) {
+    if (start_byte > 0 && start_byte <= 256) {
+        return 1;
+    } else if ((start_byte & 0b11000000) != (start_byte & 0b10000000)) {
+        return 2;
+    } else {
+        return -1;
+    }
+}
 
-    int32_t ret = 0;
-    char str[] = "abcd";
-    ret = capitalize_ascii(str);
-    printf("Capitalized String: %s\nCharacters updated: %d\n", str, ret);
+int32_t utf8_strlen(char str[]) {
+    int len = 0;
+    for (int i = 0; i < strlen(str); i += 1) {
+        if ((str[i] > 0 && str[i] <= 256) || (str[i] & 0b11000000) != (str[i] & 0b10000000)) {
+            len += 1;
+        }
+    }
+    return len;
+}
+
+int32_t codepoint_index_to_byte_index(char str[], int32_t cpi) {
+    int index = 0;
+    for (int i = 0; i < cpi; i += 1) {
+        if (str[i] > 0 && str[i] <= 256) {
+            index += 1;
+        } else if ((str[i] & 0b11000000) != (str[i] & 0b10000000)) {
+            index += 2;
+        }
+    }
+    return index;
+    // "Returns -1 if there are any errors encountered in processing the UTF-8 string."
+}
+
+//FIXME - #4 Milestone 2
+void utf8_substring(char str[], int32_t cpi_start, int32_t cpi_end, char result[]) {
+    int len = cpi_end - cpi_start;
+    char temp[len];
+    for (int i = 0; i < len; i++) {
+        temp[i] = str[i + cpi_start];
+    }
+    str = temp;
+}
+
+int32_t codepoint_at(char str[], int32_t cpi) {
+    return str[cpi + 1];
+}
+
+//FIXME: #2 Milestone 3
+char is_animal_emoji_at(char str[], int32_t cpi) {
+
+}
+
+int main() {
+    char str[] = "Joséph";
+    printf("Length of string %s is %d\n", str, utf8_strlen(str));  // 6 codepoints, (even though 7 bytes)
+
+    int32_t idx = 4;
+    printf("Codepoint index %d is byte index %d\n", idx, codepoint_index_to_byte_index("Joséph", idx));
+
+//FIXME - #4 Milestone 2
+    char result[17];
+    utf8_substring("🦀🦮🦮🦀🦀🦮🦮", 3, 7, result);
+    printf("String: %s\nSubstring: %s", result); // these emoji are 4 bytes long
+
+    printf("Codepoint at %d in %s is %d\n", idx, str, codepoint_at(str, idx)); // 'p' is the 4th codepoint
 }
---
+++ utf8analyzer.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+}
